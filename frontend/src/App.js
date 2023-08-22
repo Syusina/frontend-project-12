7e690aa07ef3a8279ed43e3a3efaface9ce4f120
Syusina Daria <syusinad@mail.ru>
2023-08-22 14:04:34 +0500

    add rollbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import i18n from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import LoginPage from './Components/LoginPage.jsx';
 import Chat from './Components/Chat.jsx';
 import Navigation from './Components/Navigation.jsx';
@@ -24,37 +26,46 @@ i18n.use(initReactI18next).init({
   fallbackLng: 'ru',
 });
 
-const App = () => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <SocketContext.Provider value={sockets}>
+const censorshipDictionaryRu = leoProfanity.getDictionary('ru');
+leoProfanity.add(censorshipDictionaryRu);
 
-        <AuthProvider>
-        
-          <div className="d-flex flex-column h-100">
-            <Router>
-              <Navigation />
-              <Routes>
-                <Route path={routes.loginPagePath()} element={<LoginPage />} />
-                <Route path="*" element={<PageNotFound />} />
-                <Route path={routes.signupPagePath()} element={<SignUp />} />
-                <Route
-                  path={routes.chatPagePath()}
-                  element={(
-                    <PrivateRoute>
-                      <Chat />
-                    </PrivateRoute>
-                  )}
-                />
-              </Routes>
-            </Router>
-          </div>
-          <div className="Toastify"><ToastContainer /></div>
-      </AuthProvider>
-    </SocketContext.Provider>
-  </I18nextProvider>
-</Provider>
-  
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  environment: 'dev',
+};
+
+const App = () => (
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <SocketContext.Provider value={sockets}>
+            <AuthProvider>
+              <div className="d-flex flex-column h-100">
+                <Router>
+                  <Navigation />
+                  <Routes>
+                    <Route path={routes.loginPagePath()} element={<LoginPage />} />
+                    <Route path="*" element={<PageNotFound />} />
+                    <Route path={routes.signupPagePath()} element={<SignUp />} />
+                    <Route
+                      path={routes.chatPagePath()}
+                      element={(
+                        <PrivateRoute>
+                          <Chat />
+                        </PrivateRoute>
+                      )}
+                    />
+                  </Routes>
+                </Router>
+              </div>
+              <div className="Toastify"><ToastContainer /></div>
+            </AuthProvider>
+          </SocketContext.Provider>
+        </I18nextProvider>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
 
 export default App;
