b13ab3ba151eda52932c466e0d5db296b2cff61b
Syusina Daria <syusinad@mail.ru>
2023-08-30 12:51:58 +0500

    change linter

--- frontend/src/Components/Messages.jsx
+++ frontend/src/Components/Messages.jsx
@@ -15,18 +15,6 @@ const Messages = () => {
     .filter(({ channelId }) => channelId === currentChannelId);
   const currentChannel = channels.find(({ id }) => id === currentChannelId);
   const countMessages = currentChannelMessages.length;
-  let messagesText;
-
-  if (countMessages === 0) {
-    messagesText = 'message_zero';
-  } else if (countMessages === 1) {
-    messagesText = 'message_one';
-  } else if (countMessages === 2 || countMessages === 3 || countMessages === 4) {
-    messagesText = 'message_few';
-  } else {
-    messagesText = 'message_many';
-  }
-
   const { t } = useTranslation();
 
   return (
@@ -34,7 +22,7 @@ const Messages = () => {
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0"><b>{`# ${currentChannel?.name}`}</b></p>
-          <span className="text-muted">{`${countMessages} ${t(`chat.${messagesText}`)}`}</span>
+          <span className="text-muted">{`${countMessages} ${t('chat.message', { count: currentChannelMessages.length })}`}</span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5 ">
           {currentChannelMessages.map(({ body, username, id }) => (
